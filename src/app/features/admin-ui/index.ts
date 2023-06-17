1938097e5a55c424acdab5a62eb67971409ccc5d
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-06-17 15:54:50 +0530

    :recycle: refactor:refactored code and files architecture

--- src/app/features/admin-ui/index.ts
+++ src/app/features/admin-ui/index.ts
@@ -1,3 +1,4 @@
 export * from './admin-ui.component';
 export * from './edit-user-dialog/edit-user-dialog.component';
 export * from './delete-user-dialog/delete-user-dialog.component';
+export * from './users-list/users-list.component';
---
+++ src/app/models/user-details.model.ts
@@ -0,0 +1,7 @@
+export interface UserDetailsModel {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  selected?: boolean;
+}
